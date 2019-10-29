818347434a03a3c32e92ccd7b492b5ae41ace7c5
Lolu Aguda <42425277+loagsword@users.noreply.github.com>
2019-10-28 21:59:06 -0400

    Fix: Modified addition test to load data from test file

--- src/CalculatorTests.py
+++ src/CalculatorTests.py
@@ -1,11 +1,13 @@
 import unittest
 from Calculator import Calculator
+from csvReader import CsvReader
 
 
 class MyTestCase(unittest.TestCase):
 
     def setUp(self) -> None:
         self.calculator = Calculator()
+        self.test_data = CsvReader('src/td/addition.csv').data
 
     def test_instantiate_calculator(self):
         self.assertIsInstance(self.calculator, Calculator)
@@ -14,8 +16,10 @@ class MyTestCase(unittest.TestCase):
         self.assertEqual(self.calculator.result, 0)
 
     def test_add_method_calculator(self):
-        self.assertEqual(self.calculator.add(2, 2), 4)
-        self.assertEqual(self.calculator.result, 4)
+        self.test_data = CsvReader('src/td/addition.csv').data
+        for row in self.test_data:
+            self.assertEqual(self.calculator.add(row['Value 1'], row['Value 2']), int(row['Result']))
+            self.assertEqual(self.calculator.result, int(row['Result']))
 
     def test_subtract_method_calculator(self):
         self.assertEqual(self.calculator.subtract(2, 2), 0)
