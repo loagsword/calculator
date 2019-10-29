841e566d78fd3fd9a1920dd4c9b8e67a9deab4f0
Lolu Aguda <42425277+loagsword@users.noreply.github.com>
2019-10-28 21:26:43 -0400

    Feature: Setup csv reader to return row data as objects

--- src/csvReader.py
+++ src/csvReader.py
@@ -8,7 +8,13 @@ class csvReader:
 
     def __init__(self, filepath):
         with open(filepath) as text_data:
-            csv_data = csv.DictReader(text_data, delimeter=',')
+            csv_data = csv.DictReader(text_data, delimiter=',')
             for row in csv_data:
                 self.data.append(row)
         pass
+
+    def return_data_as_objects(self, class_name):
+        objects = []
+        for row in self.data:
+            objects.append(classFactory(class_name, row))
+        return objects
--- src/csvTests.py
+++ src/csvTests.py
@@ -1,10 +1,19 @@
 import unittest
 from csvReader import csvReader, classFactory
+from pprint import pprint
+
 
 class MyTestCase(unittest.TestCase):
 
     def setUp(self) -> None:
         self.csv_reader = csvReader('src/td/addition.csv')
+
+    def test_return_data_as_objects(self):
+        values = self.csv_reader.return_data_as_objects('a_value')
+        self.assertIsInstance(values, list)
+        test_class = classFactory('a_value', self.csv_reader.data[0])
+        for a_value in values:
+            self.assertEqual(a_value[0], test_class[0])
 
 
 if __name__ == '__main__':
