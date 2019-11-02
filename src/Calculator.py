df04b1eb2a13b03426698afe7aee17c12592b944
Lolu Aguda <42425277+loagsword@users.noreply.github.com>
2019-11-01 21:54:02 -0400

    Feature: linked calcOperations to calculator and modified calculator class methods to call calcOperations

--- src/Calculator.py
+++ src/Calculator.py
@@ -1,4 +1,4 @@
-import calcOperations
+import calcOperations as calc
 
 
 class Calculator():
@@ -8,25 +8,25 @@ class Calculator():
         pass
 
     def add(self, a, b):
-        self.result = addition(a, b)
+        self.result = calc.addition(a, b)
         return self.result
 
     def subtract(self, a, b):
-        self.result = subtraction(a, b)
+        self.result = calc.subtraction(a, b)
         return self.result
 
     def divide(self, a, b):
-        self.result = division(a, b)
+        self.result = calc.division(a, b)
         return self.result
 
     def multiply(self, a, b):
-        self.result = multiplication(a, b)
+        self.result = calc.multiplication(a, b)
         return self.result
 
     def square(self, a):
-        self.result = square(a)
+        self.result = calc.square(a)
         return self.result
 
     def square_root(self, a):
-        self.result = square_root(a)
+        self.result = calc.square_root(a)
         return self.result
